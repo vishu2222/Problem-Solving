769718669a1e009789b8939698b4fe064b10c4ac
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-05-05 11:54:17 +0530

    remove semicolumn

--- leetCode/53.MaxSubArr.B75.js
+++ leetCode/53.MaxSubArr.B75.js
@@ -3,29 +3,29 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-  if (nums.length === 1) return nums[0];
-  let pointer1 = 0;
-  let pointer2 = 1;
-  let sum = nums[pointer1];
-  let max = nums[0];
-  let subArrBounds = [0, 0];
+  if (nums.length === 1) return nums[0]
+  let pointer1 = 0
+  let pointer2 = 1
+  let sum = nums[pointer1]
+  let max = nums[0]
+  let subArrBounds = [0, 0]
 
   for (let i = 0; i < nums.length; i++) {
-    sum = sum + nums[pointer2];
+    sum = sum + nums[pointer2]
 
     if (sum > nums[pointer2]) {
-      pointer2 = pointer2 + 1;
+      pointer2 = pointer2 + 1
     } else {
-      pointer1 = pointer2;
-      pointer2 = pointer2 + 1;
-      sum = nums[pointer1];
+      pointer1 = pointer2
+      pointer2 = pointer2 + 1
+      sum = nums[pointer1]
     }
 
     if (max < sum) {
-      max = sum;
-      subArrBounds = [pointer1, pointer2];
+      max = sum
+      subArrBounds = [pointer1, pointer2]
     }
   }
 
-  return max;
-};
+  return max
+}
